385227a95330ac2f2cf2a9401de456716ea6cfee
Zack Stout <zack@screenfeed.com>
2022-12-08 11:34:31 -0600

    Day 8

--- 7/script.js
+++ 7/script.js
@@ -1,11 +1,109 @@
 
-  const { data } = require('./input');
-  const lines = data.split('\n');
+const { data } = require('./input');
+const commands = data.split('\n');
 
-  const run = () => {
+// const test = {
+//   name: "/",
+//   children: [
+//     {
+//       name: "a",
+//       children: []
+//     },
+//     {
+//       name: "b.txt",
+//       size: 1000
+//     }
+//   ]
+// }
 
-  };
-  console.time('run');
-  console.log(run());
-  console.timeEnd('run');
-  
+const testCommands = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`.split("\n");
+
+/**
+ * Jump in difficulty!
+ * 
+ * Hmm.... not sure what's going wrong in real case. Test works.
+ * 
+ * Huh....dir pwcvj appears inside of itself.... That must be it...
+ * Name of a dir needs to depend on entire path, not just its name...
+ * 
+ * Ugh, kinda breaks whole idea, where we just try to find index of `$ cd ${dir}`. If there are many places.
+ * 
+ * Seems we actually need to keep track of working directory as we move around.
+ * 
+ * Huh... So we want to process the lines one at a time.
+ * 
+ * But then....how to keep track of each dir's cumulative size..... This is tough.
+ * 
+ * Like you'd have to, if you just go through line by line...
+ * Add the value to EACH of the ancestor divs.
+ * 
+ * Ok so map would have keys like "/", "/-a", "/-a-b", etc.
+ */
+
+/**
+ * Take name of dir. Find where we cd {dir} && ls.
+ * Next lines
+ */
+
+// const allSizes = [];
+const processedDirs = new Map();
+
+const getSize = (dir) => {
+  if (processedDirs.has(dir)) return processedDirs.get(dir);
+
+  const i = commands.findIndex(x => x === `$ cd ${dir}`);
+  const i2 = commands.slice(i + 1).findIndex(x => x.startsWith("$ cd"));
+  // console.log(dir, i, i2);
+  const contents = i2 === -1 ? commands.slice(i + 2) : commands.slice(i + 2, i + i2 + 1);
+  // console.log(contents);
+  // return contents;
+  let size = 0;
+  contents.forEach(c => {
+    if (c.startsWith("dir")) {
+      // console.log("new dir is", c.split(' ')[1])
+      size += getSize(c.split(" ")[1]);
+    } else {
+      const n = +c.match(/(\d+)/)[0];
+      // console.log(n);
+      size += n;
+    }
+  });
+
+  // console.log(`Size of ${dir} is ${size}`);
+  // allSizes.push(size);
+  processedDirs.set(dir, size);
+  return size;
+};
+
+const run = () => {
+  getSize("/");
+
+  return [...processedDirs.values()].filter(x => x <= 100000).reduce((sum, v) => sum + v, 0);
+};
+
+console.time('run');
+console.log(run());
+console.timeEnd('run');
+
--- 8/script.js
+++ 8/script.js
@@ -1,11 +1,146 @@
 
-  const { data } = require('./input');
-  const lines = data.split('\n');
+const { data } = require('./input');
+const lines = data.split('\n').map(l => l.split('').map(n => +n));
 
-  const run = () => {
+const testLines = `30373
+25512
+65332
+33549
+35390`.split('\n').map(l => l.split('').map(n => +n));
 
-  };
-  console.time('run');
-  console.log(run());
-  console.timeEnd('run');
-  
+/**
+ * Run through all rows and cols, forward and backward.
+ * Keep map of all visible trees; only add if not already has.
+ */
+
+const run = () => {
+  const visibleTrees = new Map();
+
+  // Rows forward
+  for (let i = 0; i < lines.length; i++) {
+    const row = lines[i];
+    let block = -1;
+    for (let j = 0; j < row.length; j++) {
+      const val = row[j];
+      const key = `${i},${j}`;
+      if (val > block) {
+        block = val;
+        if (!visibleTrees.has(key)) {
+          visibleTrees.set(key, 1);
+        }
+      }
+    }
+  }
+
+  // Rows backward
+  for (let i = 0; i < lines.length; i++) {
+    const row = lines[i];
+    let block = -1;
+    for (let j = row.length - 1; j >= 0; j--) {
+      const val = row[j];
+      const key = `${i},${j}`;
+      if (val > block) {
+        block = val;
+        if (!visibleTrees.has(key)) {
+          visibleTrees.set(key, 1);
+        }
+      }
+    }
+  }
+
+  // Cols forward
+  for (let j = 0; j < lines[0].length; j++) {
+    const col = lines.map(l => l[j]);
+    let block = -1;
+    for (let i = 0; i < col.length; i++) {
+      const val = col[i];
+      const key = `${i},${j}`;
+      if (val > block) {
+        block = val;
+        if (!visibleTrees.has(key)) {
+          visibleTrees.set(key, 1);
+        }
+      }
+    }
+  }
+
+  // Cols backward
+  for (let j = 0; j < lines[0].length; j++) {
+    const col = lines.map(l => l[j]);
+    let block = -1;
+    for (let i = col.length - 1; i >= 0; i--) {
+      const val = col[i];
+      const key = `${i},${j}`;
+      if (val > block) {
+        block = val;
+        if (!visibleTrees.has(key)) {
+          visibleTrees.set(key, 1);
+        }
+      }
+    }
+  }
+
+  console.log(visibleTrees);
+  return visibleTrees.size;
+};
+
+const findScore = (i, j) => {
+
+
+  const val = lines[i][j];
+
+  // console.log("finding score", val, i, j);
+
+  let x = 0, y = 0, z = 0, w = 0;
+
+  let r = i + 1, c = j;
+  while (r < lines.length) {
+    x++;
+    if (lines[r][c] >= val) break;
+    r++;
+  }
+
+
+  r = i - 1, c = j;
+  while (r >= 0) {
+    y++;
+    if (lines[r][c] >= val) break;
+    r--;
+  }
+
+  r = i, c = j + 1;
+  while (c < lines[0].length) {
+    z++;
+    if (lines[r][c] >= val) break;
+    c++;
+  }
+
+  r = i, c = j - 1;
+  while (c >= 0) {
+    w++;
+    if (lines[r][c] >= val) break;
+    c--;
+  }
+
+  // console.log(x, y, z, w);
+  return x * y * z * w;
+};
+
+const runTwo = () => {
+  const scores = new Map();
+
+  // Ignore borders (score is always 0)
+  for (let i = 1; i < lines.length - 1; i++) {
+    for (let j = 1; j < lines[i].length - 1; j++) {
+      scores.set(`${i},${j}`, findScore(i, j));
+    }
+  }
+
+  // findScore(3, 2);
+  return Math.max(...scores.values());
+};
+
+console.time('run');
+console.log(runTwo());
+console.timeEnd('run');
+
