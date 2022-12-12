d9eaa800cef5402547631cc5b67de51cf8468feb
Zack Stout <zack@screenfeed.com>
2022-12-12 13:27:50 -0600

    11.1 and 12.1

--- 11/script.js
+++ 11/script.js
@@ -1,11 +1,95 @@
 
-  const { data } = require('./input');
-  const lines = data.split('\n');
+const { data } = require('./input');
+const lines = data.split('\n');
+const testLines = require("./testInput").data.split('\n');
 
-  const run = () => {
+// const monkeys = [
+//   {
+//     items: [],
+//     op: "",
+//     test: 7,
+//     throw: [6, 7]
+//   }
+// ];
 
-  };
-  console.time('run');
-  console.log(run());
-  console.timeEnd('run');
-  
+const parseInput = (lines) => {
+  const monkeys = [];
+  // let monkeyNumber = 0;
+  let monkey = {};
+
+  // console.log("lines..", lines.slice(0, 10).map(l => l.trim()));
+
+  lines.forEach(line => {
+    const l = line.trim();
+    if (l.startsWith("Monkey")) {
+      if (monkey.hasOwnProperty("items")) monkeys.push({ ...monkey });
+
+      monkey = {};
+      // monkeyNumber = +l.match(/\d+/);
+    }
+    if (l.startsWith("Starting")) {
+      monkey.items = l.split(':')[1].split(", ").map(x => +x);
+    }
+
+    if (l.startsWith("Operation")) {
+      monkey.op = l.split("=")[1].trim();
+    }
+    if (l.startsWith("Test")) {
+      monkey.test = +l.match(/\d+/);
+    }
+    if (l.startsWith("If true")) {
+      monkey.throw = [+l.match(/\d+/)];
+    }
+    if (l.startsWith("If false")) {
+      monkey.throw.push(+l.match(/\d+/));
+    }
+  });
+
+  monkeys.push(monkey);
+
+  return monkeys;
+};
+
+const run = () => {
+  const monkeys = parseInput(lines).map(m => ({ ...m, actions: 0 }));
+  console.log("Monkeys", monkeys);
+
+  const numRounds = 20;
+
+  for (let i = 0; i < numRounds; i++) {
+    monkeys.forEach(monkey => {
+
+      while (monkey.items.length > 0) {
+        let v = monkey.items.shift();
+        // TODO: apply OP to v
+        // Ew lol
+        // if (monkey.op === "old * 19") v *= 19;
+        // if (monkey.op === "old + 6") v += 6;
+        // if (monkey.op === "old + 3") v += 3;
+        if (monkey.op === "old * old") {
+          v *= v;
+        } else {
+          const val = +monkey.op.match(/\d+/);
+          if (monkey.op.includes("*")) {
+            v *= val;
+          } else {
+            v += val;
+          }
+        }
+
+        v = Math.floor(v / 3);
+        const targetIdx = (v % monkey.test === 0) ? monkey.throw[0] : monkey.throw[1];
+        const target = monkeys[targetIdx];
+        target.items.push(v);
+        monkey.actions += 1;
+      }
+    });
+  }
+
+  return monkeys.sort((a, b) => a.actions - b.actions).map(x => x.actions);;
+};
+
+console.time('run');
+console.log(run());
+console.timeEnd('run');
+
--- 12/script.js
+++ 12/script.js
@@ -1,11 +1,97 @@
 
-  const { data } = require('./input');
-  const lines = data.split('\n');
+const { data } = require('./input');
+const lines = data.split('\n').map(x => x.trim());
 
-  const run = () => {
+const testLines =
+  `Sabqponm
+  abcryxxl
+  accszExk
+  acctuvwj
+  abdefghi`.split('\n').map(x => x.trim());
 
-  };
-  console.time('run');
-  console.log(run());
-  console.timeEnd('run');
-  
+
+// ooooohduh.... don't mark as visitedandforget...
+// bc could be visitable from another dir
+// noo....nvm
+
+
+const getNeighbors = (r, c) => {
+  const result = [];
+
+  if (r > 0) result.push({ r: r - 1, c });
+  if (r < lines.length - 1) result.push({ r: r + 1, c });
+  if (c > 0) result.push({ r, c: c - 1 });
+  if (c < lines[0].length - 1) result.push({ r, c: c + 1 });
+  return result;
+};
+
+const visited = new Map();
+
+const canVisit = (srcVal, r, c) => {
+  if (srcVal === "S") return true;
+  if (visited.has(`${r},${c}`)) return false;
+  // if (lines[r][c] === "E") return true;
+  if (lines[r][c] === "E") return srcVal === "z";
+
+  // return true;
+  // console.log("can visit..", lines[r][c].charCodeAt(0), srcVal.charCodeAt(0) + 1);
+  return lines[r][c].charCodeAt(0) <= srcVal.charCodeAt(0) + 1;
+};
+
+const run = () => {
+  const start = { r: 0, c: 0 };
+  const end = { r: 0, c: 0 };
+
+  start.r = lines.findIndex(x => x.includes("S"));
+  start.c = lines[start.r].split('').findIndex(x => x === "S");
+  end.r = lines.findIndex(x => x.includes("E"));
+  // console.log("row...", lines[end.r]);
+  end.c = lines[end.r].split('').findIndex(x => x === "E");
+
+  // If char is 'd', can only visit 'e' and below
+  console.log(start, end, 'a'.charCodeAt(0), 'z'.charCodeAt(0)); // 97 to 122
+
+  visited.set(`${start.r},${start.c}`, 0);
+  // const target = `${end.r},${end.c}`;
+
+  // let i = 0;
+
+  while (true) {
+    // i += 1;
+    const minKey = [...visited.keys()].sort((a, b) => visited.get(a) - visited.get(b))[0];
+    if (!minKey) continue;
+
+    const pos = {
+      r: +minKey.split(',')[0],
+      c: +minKey.split(',')[1]
+    };
+
+    // console.log("mk", minKey, pos, visited);
+
+    const val = lines[pos.r][pos.c];
+
+    const neighbors = getNeighbors(pos.r, pos.c)
+      .filter(n => canVisit(val, n.r, n.c));
+
+    // console.log(neighbors);
+
+    if (neighbors.some(n => n.r === end.r && n.c === end.c)) {
+      // Break  
+      return visited.get(minKey) + 1;
+    }
+
+    neighbors.forEach(n => {
+      // const { r, c } = n;
+      // console.log("rc", r, c, n);
+      // const val = lines[r][c];
+      visited.set(`${n.r},${n.c}`, visited.get(minKey) + 1);
+    });
+
+    visited.set(minKey, Infinity); // Should effectively set this cell as "dead", never to be chosen as new head/leaf
+  }
+};
+
+console.time('run');
+console.log("Result:", run());
+console.timeEnd('run');
+
