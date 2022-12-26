32ab96ac846078464da8100d66ce23183fc234af
Zack Stout <zack@screenfeed.com>
2022-12-26 13:12:36 -0600

    Day 23, 22 part 1

--- 22/script.js
+++ 22/script.js
@@ -1,11 +1,139 @@
 
-  const { data } = require('./input');
-  const lines = data.split('\n');
+const { data } = require('./input');
+const lines = data.split('\n');
 
-  const run = () => {
+// Eeek, watch out for whitespace
+const testInput = `
+        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
 
-  };
-  console.time('run');
-  console.log(run());
-  console.timeEnd('run');
-  
+10R5L5R10L4R5L5`.split("\n");
+
+const EMPTY = "X";
+
+const parse = input => {
+  const moves = input.at(-2); // NOTE!!! was -1 for test
+  const nums = moves.split(/[RL]/);
+  const turns = moves.match(/[RL]/g);
+  const res = [];
+  // console.log("moves...", input.length, input.slice(input.length - 3));
+  for (let i = 0; i < nums.length; i++) {
+    res.push(+nums[i]);
+    if (turns[i]) res.push(turns[i]);
+  }
+  const grid = input
+    .slice(0, input.length - 2)
+    .filter(x => x)
+    // NOTE only do this for real input, not for test
+    .map((x, i) => i >= 100 ? x : "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" + x)
+    .map(l => l.replace(/\s/g, "X"));
+  const maxLen = Math.max(...grid.map(l => l.length));
+
+  grid.forEach((line, lineIdx) => {
+    while (line.length < maxLen) {
+      line += EMPTY;
+      grid[lineIdx] = line;
+    }
+  });
+
+  // console.log("end of grid", grid.slice(grid.length - 3))
+  return { moves: res, grid };
+};
+
+// Curr is in [0,1,2,3], t is in [L,R]
+const turn = (curr, t) => {
+  if (t === "L") return (curr + 4 - 1) % 4;
+  if (t === "R") return (curr + 1) % 4;
+};
+
+const DIR_DELTAS = {
+  0: [1, 0],
+  1: [0, 1],
+  2: [-1, 0],
+  3: [0, -1]
+};
+
+
+const run = () => {
+  const { moves, grid } = parse(lines);
+  // console.log(moves, grid, grid.slice(grid.length - 100));
+
+  const NUM_ROWS = grid.length;
+  const NUM_COLS = grid[0].length;
+
+  const pos = {
+    r: 0,
+    c: grid[0].indexOf(".")
+  }
+  // 0=R, 1=D, 2=L, 3=U
+  let dir = 0;
+
+
+
+  // return;
+
+  // console.log(pos);
+
+  while (moves.length > 0) {
+    let next = moves.shift();
+
+    // console.log("next", next);
+    if (next === parseInt(next)) {
+      // Move in current dir. If hit wall ("#"), stop. If hit edge (EMPTY), wrap.
+      // Look at "target" cell, including wrapping -- if a wall, don't go there.
+
+      let targetCell = { ...pos };
+      const delta = DIR_DELTAS[dir];
+      // console.log("DELTA", delta);
+
+      // Walk for {next} steps, or until hit a wall.
+      while (next > 0) {
+
+        targetCell.c += delta[0];
+        targetCell.r += delta[1];
+
+        // If target is an "X", keep going until it's NOT an "X"
+        while (grid[targetCell.r]?.[targetCell.c] === "X" || grid[targetCell.r]?.[targetCell.c] === undefined) {
+          targetCell.r = (targetCell.r + delta[1] + NUM_ROWS) % NUM_ROWS;
+          targetCell.c = (targetCell.c + delta[0] + NUM_COLS) % NUM_COLS;
+          const val = grid[targetCell.r][targetCell.c];
+          if (val === "#" || val === ".") break;
+          // if (val === EMPTY) break;
+        }
+
+        const val = grid[targetCell.r][targetCell.c];
+        if (val === "#") {
+          break;
+        }
+        pos.r = targetCell.r;
+        pos.c = targetCell.c;
+
+        // console.log("Pos", pos.r, pos.c);
+        next--;
+      }
+    } else {
+      // Change dir
+      dir = turn(dir, next);
+    }
+  }
+
+  console.log("Final pos", pos, "and dir", dir);
+  return 1000 * (pos.r + 1) + 4 * (pos.c + 1) + dir;
+
+  // console.log(turn(0, "R"), turn(0, "L"), turn(2, "R"))
+};
+
+console.time('run');
+console.log(run());
+console.timeEnd('run');
+
--- 23/script.js
+++ 23/script.js
@@ -1,11 +1,143 @@
 
-  const { data } = require('./input');
-  const lines = data.split('\n');
+const { data } = require('./input');
+const lines = data.split('\n');
 
-  const run = () => {
+const test = `....#..
+  ..###.#
+  #...#.#
+  .#...##
+  #.###..
+  ##.#.##
+  .#..#..`.split('\n').map(x => x.trim());
 
-  };
-  console.time('run');
-  console.log(run());
-  console.timeEnd('run');
-  
+
+const testSmall = `.....
+  ..##.
+  ..#..
+  .....
+  ..##.
+  .....`.split('\n').map(x => x.trim());
+
+const parse = arr => {
+  const m = new Map();
+  arr.forEach((line, r) => {
+    line.split("").forEach((char, c) => {
+      if (char === "#") {
+        m.set(`${r},${c}`, 1);
+      }
+    });
+  });
+  return m;
+};
+
+const DIRS_DELTAS = {
+  N: [[0, -1], [1, -1], [-1, -1]],
+  E: [[1, 0], [1, 1], [1, -1]],
+  S: [[0, 1], [1, 1], [-1, 1]],
+  W: [[-1, 0], [-1, 1], [-1, -1]],
+};
+
+let DIRS = ["N", "S", "W", "E"];
+
+const cloneMap = source => new Map(JSON.parse(JSON.stringify(Array.from(source))));
+
+const mapsEqual = (m1, m2) => {
+  if (!m1 || !m2) return false;
+  return [...m1.keys()].every(key => m1.get(key) === m2.get(key));
+};
+
+/**
+ * Create a new targets Map to track where each elf wants to go.
+ * (Keyed by target cell, value is array of elves that want to go there)
+ * If any elves have 0 neighbors, skip them.
+ * Move all elves in targets Map with single value in array to target.
+ */
+const step = m => {
+
+  const targets = new Map();
+
+  const result = cloneMap(m);
+
+  // Populate targets map
+  [...m.keys()].forEach(elf => {
+    const [r, c] = elf.split(",").map(Number);
+    const deltas = [
+      [1, 0], [-1, 0],
+      [0, 1], [1, 1], [-1, 1],
+      [0, -1], [1, -1], [-1, -1]
+    ];
+    const neighbors = deltas.map(d => ({ r: r + d[1], c: c + d[0] })).filter(pos => m.has(`${pos.r},${pos.c}`));
+    if (neighbors.length === 0) return;
+
+
+    let isDone = false;
+    DIRS.forEach(direction => {
+      if (isDone) return;
+      const dirDeltas = DIRS_DELTAS[direction];
+      // if any of those cells are occupied, skip to next direction.
+      if (dirDeltas.some(d => m.has(`${r + d[1]},${c + d[0]}`))) {
+        return;
+      } else {
+        // Set that direction as target
+        // const key = "";
+        const move = dirDeltas[0];
+        const key = `${r + move[1]},${c + move[0]}`;
+        if (targets.has(key)) {
+          targets.set(key, [...targets.get(key), elf])
+        } else {
+          targets.set(key, [elf]);
+        }
+        isDone = true;
+      }
+    });
+  });
+  // console.log("targets", targets);
+
+  targets.forEach((value, key) => {
+    if (value.length === 1) {
+      // Move it
+      result.delete(value[0]);
+      result.set(key, 1);
+    }
+  });
+
+  // console.log("New resultap", result);
+  return result;
+
+};
+
+const run = () => {
+  // console.log(test, parse(test));
+
+  let m = parse(lines.map(x => x.trim()));
+  // let m = parse(test);
+  // Step until no elves move (maps are equal)
+  // Just manually update the DIRS array here (DIRS.push(DIRS.shift()))
+
+  let prev = null;
+  let i = 0;
+  while (true && i < 500000) {
+    if (mapsEqual(m, prev)) break;
+    prev = m;
+    m = step(m);
+    i++;
+    DIRS.push(DIRS.shift());
+  }
+
+  const maxR = Math.max(...[...m.keys()].map(key => +key.split(",")[1]));
+  const minR = Math.min(...[...m.keys()].map(key => +key.split(",")[1]));
+  const maxC = Math.max(...[...m.keys()].map(key => +key.split(",")[0]));
+  const minC = Math.min(...[...m.keys()].map(key => +key.split(",")[0]));
+
+  console.log("Num steps", i);
+  console.log("Bounds", minR, maxR, minC, maxC);
+
+  // Lol it's pretty sneaky that in example, you get same result if you leave out the "plus 1" and the "minus m.size"
+  return (maxR - minR + 1) * (maxC - minC + 1) - m.size;
+};
+
+// Uh oh... part 2.... took 9 seconds to run 1000.... Oh good it's just 1069
+console.time('run');
+console.log(run());
+console.timeEnd('run');
+
